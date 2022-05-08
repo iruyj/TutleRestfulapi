c0a0b09590239a6397254f6d4547e67d2f975c62
Jyuri <jeon1310@gmail.com>
2022-05-08 22:52:49 +0900

    cure app create

--- turtleapiserver/urls.py
+++ turtleapiserver/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
-
+# Serializers : 데이터를 json형태로 만들어주는 역할
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
+        # 해당 컬럼만 json형태로 만든다는 말
         fields = ['url', 'username', 'email', 'is_staff']
 
 
@@ -18,7 +19,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet)  # users 호출하면 ViewSet 실행
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
