6f924d9bf9f8ebece1d0a75a019d7a2763ff6c45
Jyuri <jeon1310@gmail.com>
2022-05-15 22:55:15 +0900

    project name edit

--- server_dev/wsgi.py
+++ server_dev/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for turtleapiserver project.
+WSGI config for server_dev project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turtleapiserver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_dev.settings')
 
 application = get_wsgi_application()
