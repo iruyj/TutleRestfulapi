24e43db8674783fa825bfec18fa10c7b9fa18c11
Jyuri <jeon1310@gmail.com>
2022-05-22 14:09:07 +0900

    db rollback

--- tutlerestfulapi/settings.py
+++ tutlerestfulapi/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'tutlerestfulapi'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '107.21.77.37',
-        'NAME': 'mysql-db',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
