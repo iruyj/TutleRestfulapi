9ff8a4d388a5d03904b428919d55c4f5c2683f54
Jyuri <jeon1310@gmail.com>
2022-06-01 22:11:17 +0900

    reset log

--- cure/urls.py
+++ cure/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from django.conf.urls import url
 from cure import views
-from cure.views import all_log
+# from cure.views import all_log
 
 app_name = "cures"
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('cures/user/',views.CureSelect.as_view(),name='cures_user'),
-    # path('cures/today/',views.TodaySelect.as_view(),name='cures_today'),
-    # path('new/',views.CureCreate.as_view(),name='new'),
-    path('log/',all_log ,name='all'),
+    path('cures/user/',views.CureSelect.as_view(),name='cures_user'),
+    path('cures/today/',views.TodaySelect.as_view(),name='cures_today'),
+    path('new/',views.CureCreate.as_view(),name='new'),
+    # path('log/',all_log ,name='all'),
 ]
--- cure/views.py
+++ cure/views.py
@@ -10,54 +10,54 @@ from .models import Cure
 from .serializers import CureSerializer
 # Create your views here.
 
-# class CureCreate(generics.GenericAPIView,
-#                  mixins.CreateModelMixin,
-#                  mixins.ListModelMixin):
-#     def post(self, request):
-#         created = request.data.get('created',"")
-#         start = request.data.get('start',"")
-#         end = request.data.get('end',"")
-#         status = request.data.get('status',"")
-#         stretch = request.data.get('stretch',"")
-#         user_email = request.data.get('user_email',"")
-#
-#         cure = Cure.objects.create(
-#             created= created,
-#             start=start,
-#             end=end,
-#             status=status,
-#             stretch=stretch,
-#             user_email=user_email
-#         )
-#         return Response(dict(msg="스트레칭log 생성 완료", name=cure.created, start_date=cure.start.strftime('%Y-%m-%d'), end_date=cure.created))
-#
-# # Select this user stretchLogs
-# class CureSelect(APIView):
-#     def post(self, request):
-#         user_email = request.data.get('user_email', "")
-#
-#         cures = Cure.objects.filter(email=user_email)
-#         cure_list = []
-#         for cure in cures:
-#             cure_list.append(dict(email=cure.email, start_time=cure.start, end_time=cure.end, state=cure.state,stretch=cure.stretch, created=cure.created))
-#
-#         return Response(dict(cures=cure_list))
-#
-# # Select today's stretchLogs
-# class TodaySelect(APIView):
-#     def get(self, request):
-#         today = datetime.date
-#         user_email = request.data.get('user_email', "")
-#
-#         cures = Cure.objects.filter(created=today,email=user_email)
-#         cure_list = []
-#         for cure in cures:
-#             cure_list.append(dict(email=cure.email, start_time=cure.start, end_time=cure.end, state=cure.state,stretch=cure.stretch, created=cure.created))
-#
-#         return Response(dict(cures=cure_list))
+class CureCreate(generics.GenericAPIView,
+                 mixins.CreateModelMixin,
+                 mixins.ListModelMixin):
+    def post(self, request):
+        created = request.data.get('created',"")
+        start = request.data.get('start',"")
+        end = request.data.get('end',"")
+        status = request.data.get('status',"")
+        stretch = request.data.get('stretch',"")
+        user_email = request.data.get('user_email',"")
 
-@api_view(['GET'])
-def all_log(request):
-    all = Cure.objects.all()
-    serializers = CureSerializer(all, many=True)
-    return Response(serializers.data)
+        cure = Cure.objects.create(
+            created= created,
+            start=start,
+            end=end,
+            status=status,
+            stretch=stretch,
+            user_email=user_email
+        )
+        return Response(dict(msg="스트레칭log 생성 완료", name=cure.created, start_date=cure.start.strftime('%Y-%m-%d'), end_date=cure.created))
+
+# Select this user stretchLogs
+class CureSelect(APIView):
+    def post(self, request):
+        user_email = request.data.get('user_email', "")
+
+        cures = Cure.objects.filter(email=user_email)
+        cure_list = []
+        for cure in cures:
+            cure_list.append(dict(email=cure.email, start_time=cure.start, end_time=cure.end, state=cure.state,stretch=cure.stretch, created=cure.created))
+
+        return Response(dict(cures=cure_list))
+
+# Select today's stretchLogs
+class TodaySelect(APIView):
+    def get(self, request):
+        today = datetime.date
+        user_email = request.data.get('user_email', "")
+
+        cures = Cure.objects.filter(created=today,email=user_email)
+        cure_list = []
+        for cure in cures:
+            cure_list.append(dict(email=cure.email, start_time=cure.start, end_time=cure.end, state=cure.state,stretch=cure.stretch, created=cure.created))
+
+        return Response(dict(cures=cure_list))
+
+# @api_view(['GET'])
+# def all_log(request):
+#     all = Cure.objects.all()
+#     serializers = CureSerializer(all, many=True)
+#     return Response(serializers.data)
