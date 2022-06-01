ca210c05facffd9d1c198f01d48515b3488b3cfa
Jyuri <jeon1310@gmail.com>
2022-06-01 22:05:14 +0900

    app st

--- tutlerestfulapi/settings.py
+++ tutlerestfulapi/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'turtles.apps.TurtlesConfig',
-    'cure.apps.CureConfig',
     'turtles',
     'cure',
 ]
