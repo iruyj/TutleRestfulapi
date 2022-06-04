e5a6b5784fa1837c60388f1ee097252dc04029b9
Jyuri <jeon1310@gmail.com>
2022-06-04 19:51:24 +0900

    serializers.py edit

--- cure/serializers.py
+++ cure/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Cure
 
 class CureSerializer(serializers.ModelSerializer):
-    turtle = serializers.ReadOnlyField(source='turtle.email')
+    # turtle = serializers.ReadOnlyField(source='turtle.email')
 
     class Meta:
         model = Cure
-        fields = ['created','start','end','stretch','status','user_email']
+        fields = ['id','created','start','end','stretch','status','user_email']
